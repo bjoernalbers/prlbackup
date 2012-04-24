600515b604f2558c37ad6f675e78ed3d507445cb
Björn Albers <bjoernalbers@googlemail.com>
2012-04-24 23:32:05 +0200

    Update dev steps

--- features/dev_steps.feature
+++ features/dev_steps.feature
@@ -1,29 +1,42 @@
 Feature: Development Steps
 
   In order to keep my cucumber scenarios nice and tight
-  As a developer of Parallels-Backup
+  As a developer of prlbackup
   I want convenient development steps to fake virtual machines
 
-  Scenario: `prlctl list --all`
+  Background:
     Given the following virtual machines:
       | uuid                                   | status  | name          |
       | {97351580-afd7-4aff-9960-814196b28e37} | stopped | Mac OS X Lion |
       | {423dba54-45e3-46f1-9aa2-87d61ce6b757} | running | Windows XP    |
       | {55aae003-298d-4199-82ed-23658a218605} | stopped | Ubuntu        |
+
+  Scenario: prlctl list --info Ubuntu
+    When I successfully run `prlctl list --info Ubuntu`
+    Then the stdout should contain exactly:
+      """
+      ID: {55aae003-298d-4199-82ed-23658a218605}
+      Name: Ubuntu
+      State: stopped
+
+      """
+
+  Scenario: prlctl backup Ubuntu
+    When I successfully run `prlctl backup Ubuntu`
+    Then the stdout should contain exactly:
+      """
+      Backing up the VM Ubuntu
+      The virtual machine has been successfully backed up with backup id {d51e6df1-83e9-46e2-aef1-3807d721c1be}.
+
+      """
+
+  Scenario: `prlctl list --all`
     When I run `prlctl list --all --output uuid`
-    Then the stdout should contain:
+    Then the stdout should contain exactly:
       """
       UUID
-      """
-    And the stdout should contain:
-      """
       {97351580-afd7-4aff-9960-814196b28e37}
-      """
-    And the stdout should contain:
-      """
       {423dba54-45e3-46f1-9aa2-87d61ce6b757}
-      """
-    And the stdout should contain:
-      """
       {55aae003-298d-4199-82ed-23658a218605}
+
       """
--- features/steps/dev_steps.rb
+++ features/steps/dev_steps.rb
@@ -4,32 +4,39 @@ end
 
 Given /^the following virtual machines?:$/ do |vm_table|
   format = "%-40s%-13s%-16s%s"
-  all_vm = [format % %w{UUID STATUS IP_ADDR NAME"}]
+  #all_vm = [format % %w{UUID STATUS IP_ADDR NAME"}]
+  all_vm = %w[UUID]
   vm_table.hashes.each do |vm|
-    name, uuid = vm["name"], vm["uuid"]
-    all_vm << format % [vm["uuid"], vm["status"], "-", vm["name"]]
+    name, uuid, status  = vm["name"], vm["uuid"], vm["status"]
+    #all_vm << format % [vm["uuid"], vm["status"], "-", vm["name"]]
+    all_vm << vm['uuid']
     steps %Q{
-      Given I could run `prlctl list --info "#{name}"` with stdout:
+      Given I double `prlctl list --info "#{name}"` with stdout:
+        """
+        ID: #{uuid}
+        Name: #{name}
+        State: #{status}
+        """
+      And I double `prlctl list --info #{uuid}` with stdout:
         """
         ID: #{uuid}
         Name: #{name}
         State: running
         """
-      And I could run `prlctl list --info #{uuid}` with stdout:
-        """
-        ID: #{uuid}
-        Name: #{name}
-        State: running
-        """
-      And I could run `prlctl stop #{uuid}` with stdout:
+      And I double `prlctl stop #{uuid}` with stdout:
         """
         ...successfully stopped.
         """
-      And I could run `prlctl backup #{uuid}` with stdout:
+      And I double `prlctl backup #{uuid}` with stdout:
         """
         ...successfully backed up with backup id {a3935342-e8b6-4e67-b9f6-1b2adf844837}.1
         """
-      And I could run `prlctl start #{uuid}` with stdout:
+      And I double `prlctl backup "#{name}"` with stdout:
+        """
+        Backing up the VM #{name}
+        The virtual machine has been successfully backed up with backup id {d51e6df1-83e9-46e2-aef1-3807d721c1be}.
+        """
+      And I double `prlctl start #{uuid}` with stdout:
         """
         ...successfully started.
         """
@@ -38,7 +45,7 @@ Given /^the following virtual machines?:$/ do |vm_table|
   
   all_vm = all_vm.join("\n")
   steps %Q{
-    Given I could run `prlctl list --all --output uuid` with stdout:
+    Given I double `prlctl list --all --output uuid` with stdout:
       """
 	    #{all_vm}
 	    """
